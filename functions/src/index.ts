a35aeac657975cda4c5472dba852905ece5411d1
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-12-23 15:46:38 +0800

    Add makeUppercase function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -26,3 +26,17 @@ export const addMessage = functions.https.onRequest((req, res) => {
     res.redirect(303, snapshot.ref.toString())
   })
 })
+
+// Listens for new messages added to /messages/:pushId/original and creates an
+// uppercase version of the message to /messages/:pushId/uppercase
+export const makeUppercase = functions.database.ref('/messages/{pushId}/original')
+  .onCreate((snapshot, context) => {
+    // Grab the current value of what was written to the Realtime Database.
+    const original = snapshot.val()
+    console.log('Uppercasing', context.params.pushId, original)
+    const uppercase = original.toUpperCase()
+    // You must return a Promise when performing asynchronous tasks inside a Functions such as
+    // writing to the Firebase Realtime Database.
+    // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+    return snapshot.ref.parent.child('uppercase').set(uppercase)
+  })

67cacd7695fb446176a53d4e18ff41ac6eda519c
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-12-23 14:15:32 +0800

    Setup project

---
+++ functions/src/index.ts
@@ -0,0 +1,9 @@
+import * as functions from 'firebase-functions';
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  console.log('Hello Console!')
+  response.send("Hello from another planet!");
+});
